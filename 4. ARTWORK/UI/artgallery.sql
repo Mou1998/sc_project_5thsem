53be9bdeef3ce4903c0ebdcf08edd80c7d0a8546
sanijana31 <52424404+sanijana31@users.noreply.github.com>
2021-04-29 16:25:48 +0530

    Update artgallery.sql

--- 4. ARTWORK/UI/artgallery.sql
+++ 4. ARTWORK/UI/artgallery.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 27, 2021 at 02:35 PM
+-- Generation Time: Apr 29, 2021 at 12:53 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -39,15 +39,16 @@ CREATE TABLE `artists` (
 --
 
 INSERT INTO `artists` (`name`, `dob`, `address`, `style_of_art`) VALUES
-('Anish Kapoor', '1986-08-26', 'Howrah', 'Sculpture'),
-('Antonio Lopez', '1995-05-11', 'Kolkata', 'sketch'),
-('David Downton', '1993-06-17', 'Kolkata', 'sketch'),
-('Henry Moore', '1982-07-08', 'Kolkata', 'Sculpture'),
-('Jamini Roy', '1950-01-01', 'Kolkata', 'Painting'),
-('Leonardo da Vinci', '1956-12-13', 'Howrah', 'Painting'),
-('Michelangelo', '1977-06-08', 'Kolkata', 'Sculpture'),
-('Pablo Picasso', '1960-02-25', 'Kolkata', 'Painting'),
-('Vincent van Gogh', '1975-07-09', 'Howrah', 'Painting');
+('Anish Kapoor', '1954-03-12', 'Mumbai', 'Sculpture'),
+('Jamini Roy', '1887-04-11', 'Kolkata', 'Painting'),
+('Jogen Chowdhury', '1939-02-15', 'Faridpur District, Bangladesh', 'Painting'),
+('MF Hussain', '1915-09-17', 'Pandharpur', 'Painting'),
+('Nandalal Bose', '1882-02-03', 'Kolkata', 'Painting'),
+('Nilima Sheikh', '1945-11-18', 'New Delhi', 'Miniature Painting'),
+('Poorva Shingre', '1980-12-15', 'Kolkata', 'Linocut'),
+('Sankho Chaudhuri', '1916-02-25', 'Bihar', 'Sculpture'),
+('Sayed Haider Raza', '1922-02-22', 'Madhya Pradesh', 'Painting'),
+('Subodh Gupta', '1964-01-01', 'Khagaul', 'Sculpture');
 
 -- --------------------------------------------------------
 
@@ -69,18 +70,33 @@ CREATE TABLE `artwork` (
 --
 
 INSERT INTO `artwork` (`title`, `type`, `year_of_made`, `price`, `a_name`, `txn_id`) VALUES
-('a', 'Painting', 2000, 10, 'Anish Kapoor', 21),
-('b', 'pastel', 2000, 100, 'Anish Kapoor', 22),
-('Black Horse', 'Animals', 1985, 50000, 'Jamini Roy', 16),
-('Bride and two Companions', 'Indian Calture', 1998, 100000, 'Jamini Roy', 13),
-('Cloud Gate', 'Scalpture', 2006, 500000, 'Anish Kapoor', 17),
-('Dead Girl', 'Painting', 2000, 75000, 'Antonio Lopez', 12),
-('Dirty Corner', 'Scalpture', 2011, 100000, 'Anish Kapoor', 20),
-('Krishna with Gopis', 'Indian God of Love', 1975, 100000, 'Jamini Roy', 14),
-('Sink and Mirror', 'Painting', 2010, 65000, 'Antonio Lopez', 19),
-('Sky Mirror', 'Scalpture', 2006, 1000, 'Anish Kapoor', 18),
-('The Apparition', 'Painting', 2001, 80000, 'Antonio Lopez', NULL),
-('Three Pujarinis', 'Indian Calture', 1980, 1000, 'Jamini Roy', 15);
+('Anish Kapoor', 'Scalpture', 2006, 250000, 'Anish Kapoor', NULL),
+('Ankuran', 'abstract', 1987, 43000, 'Sayed Haider Raza', NULL),
+('Bihari', 'Scalpture', 1999, 10000, 'Subodh Gupta', 29),
+('Blood on the road (4)', 'abstract', 2021, 100000, 'Nilima Sheikh', 34),
+('Blood on the road (5)', 'abstract', 2021, 100000, 'Nilima Sheikh', 32),
+('Blood on the Road 2', 'abstract', 2018, 50000, 'Nilima Sheikh', 33),
+('Couple', 'acrylic', 1982, 50000, 'Jogen Chowdhury', 35),
+('Dancing Gopi', 'acrylic', 1905, 50000, 'Jamini Roy', 31),
+('Darjeeling and Fog', 'acrylic', 1940, 129999, 'Nandalal Bose', 37),
+('Dirty Corner', 'Scalpture', 2011, 1000000, 'Anish Kapoor', 28),
+('Erasure', 'abstract', 2020, 99999, 'Nilima Sheikh', 38),
+('Ganga and Jamuna', 'Oil', 1983, 60000, 'MF Hussain', 30),
+('High Life', 'Scalpture', 2002, 50000, 'Subodh Gupta', NULL),
+('Horses', 'abstract', 1960, 45000, 'MF Hussain', NULL),
+('Krishna and Balarama', 'acrylic', 1901, 50000, 'Jamini Roy', 39),
+('Krishna with Gopis', 'acrylic', 1903, 150000, 'Jamini Roy', NULL),
+('Meerut Congress', 'Scalpture', 1975, 20000, 'Sankho Chaudhuri', NULL),
+('Mother and Child', 'acrylic', 1903, 75000, 'Jamini Roy', 40),
+('Mother Teresa', 'abstract', 1988, 84000, 'MF Hussain', 41),
+('New Clouds', 'acrylic', 1937, 120000, 'Nandalal Bose', 27),
+('Rati with her Bow', 'acrylic', 1937, 125000, 'Nandalal Bose', NULL),
+('Santhal', 'Scalpture', 1980, 40000, 'Sankho Chaudhuri', NULL),
+('Sarhad 2', 'abstract', 2018, 99999, 'Nilima Sheikh', NULL),
+('Saurashtra', 'abstract', 1983, 67000, 'Sayed Haider Raza', NULL),
+('Smoking ladies', 'Linocut', 2010, 3000, 'Poorva Shingre', NULL),
+('Three Pujarinis', 'acrylic', 1905, 100000, 'Jamini Roy', 36),
+('Tiger in the Moonlit Night', 'Oil', 1979, 100000, 'Jogen Chowdhury', NULL);
 
 -- --------------------------------------------------------
 
@@ -98,27 +114,54 @@ CREATE TABLE `classify` (
 --
 
 INSERT INTO `classify` (`a_title`, `g_name`) VALUES
-('Three Pujarinis', 'oil painting'),
-('Three Pujarinis', 'Indian history'),
-('Three Pujarinis', 'caltural'),
-('Bride and two Companions', 'oil painting'),
-('Bride and two Companions', 'Indian history'),
-('Bride and two Companions', 'caltural'),
-('Black Horse', 'oil painting'),
-('Black Horse', 'real life'),
-('Krishna with Gopis', 'oil painting'),
-('Krishna with Gopis', 'Indian history'),
-('Krishna with Gopis', 'caltural'),
+('Blood on the road (4)', 'Tempera on Sanganer paper'),
+('Blood on the road (5)', 'Tempera on Sanganer paper'),
+('Erasure', 'Mixed tempura on Sanganer handmade paper'),
+('Sarhad 2', 'Tempera on Sanganer paper'),
+('Blood on the Road 2', 'Tempera on Sanganer Vasli paper'),
+('Three Pujarinis', 'Indian Culture'),
+('Mother and Child', 'Love'),
+('Mother and Child', 'Family'),
+('Krishna and Balarama', 'Indian Culture'),
+('Krishna and Balarama', 'Indian God'),
 ('Krishna with Gopis', 'God'),
-('Cloud Gate', 'Mirror Image'),
-('Sky Mirror', 'Mirror'),
-('Dirty Corner', 'Real Object'),
-('Dead Girl', 'real life'),
-('The Apparition', 'real life'),
-('Sink and Mirror', 'Mirror Image'),
-('Sink and Mirror', 'Real Object'),
-('a', 'Real Object'),
-('b', 'real life');
+('Krishna with Gopis', 'Love'),
+('Dancing Gopi', 'Dancing'),
+('Dancing Gopi', 'Love'),
+('Smoking ladies', 'modern'),
+('Smoking ladies', 'Girl'),
+('Smoking ladies', 'New Generation'),
+('New Clouds', 'Nature'),
+('New Clouds', 'Lady'),
+('New Clouds', 'Joy'),
+('New Clouds', 'Forest'),
+('Darjeeling and Fog', 'Nature'),
+('Rati with her Bow', 'Tempera on paper'),
+('Rati with her Bow', 'Weapon'),
+('Couple', 'love'),
+('Tiger in the Moonlit Night', 'Forest'),
+('Tiger in the Moonlit Night', 'Nature'),
+('Tiger in the Moonlit Night', 'Animal'),
+('Tiger in the Moonlit Night', 'Murder'),
+('Horses', 'animal'),
+('Horses', 'warrior'),
+('Mother Teresa', 'Kind'),
+('Mother Teresa', 'lady'),
+('Mother Teresa', 'Love'),
+('Ganga and Jamuna', 'Nature'),
+('Ganga and Jamuna', 'River'),
+('Saurashtra', 'gestural strokes'),
+('Saurashtra', 'Abstractionism'),
+('Ankuran', 'Abstractionism'),
+('Anish Kapoor', 'Stainless steel'),
+('Anish Kapoor', 'City'),
+('Dirty Corner', 'Stainless steel'),
+('High Life', 'Stainless steel'),
+('Bihari', 'Rock'),
+('Bihari', 'Soil'),
+('Bihari', 'acrylic'),
+('Santhal', 'bronze'),
+('Meerut Congress', 'bronze');
 
 -- --------------------------------------------------------
 
@@ -138,10 +181,13 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`id`, `c_name`, `c_address`, `total_spent`) VALUES
-(1, 'Sani Jana', 'PO+VILL: DAKSHIN JHAPORDAH, PS: DOMJUR DIST: HOWAH', 501000),
-(2, 'Susmita', 'Domjur', 215000),
-(3, 'Dipika', 'Howrah', 101110),
-(4, 'Saikat Jana', 'Howrah', 175000);
+(5, 'Sani Jana', 'Howrah', 260000),
+(6, 'Susmita Sett', 'Howrah', 209999),
+(7, 'Saikat Jana', 'Kolkata', 245000),
+(8, 'Debopriyo Ghosh', 'Howrah', 1000000),
+(9, 'Mousumi', 'hooghly', 263999),
+(10, 'Saradindu Rana', 'midnapore', 100000),
+(11, 'Soumya Jana', 'Kolkata', 0);
 
 -- --------------------------------------------------------
 
@@ -158,14 +204,37 @@ CREATE TABLE `groups` (
 --
 
 INSERT INTO `groups` (`g_name`) VALUES
-('caltural'),
+('Abstractionism'),
+('acrylic'),
+('Animal'),
+('bronze'),
+('City'),
+('Dancing'),
+('Family'),
+('Forest'),
+('gestural strokes'),
+('Girl'),
 ('God'),
-('Indian history'),
-('Mirror'),
-('Mirror Image'),
-('oil painting'),
-('real life'),
-('Real Object');
+('Indian Culture'),
+('Indian God'),
+('Joy'),
+('Kind'),
+('Lady'),
+('Love'),
+('Mixed tempura on Sanganer handmade paper'),
+('modern'),
+('Murder'),
+('Nature'),
+('New Generation'),
+('River'),
+('Rock'),
+('Soil'),
+('Stainless steel'),
+('Tempera on paper'),
+('Tempera on Sanganer paper'),
+('Tempera on Sanganer Vasli paper'),
+('warrior'),
+('Weapon');
 
 -- --------------------------------------------------------
 
@@ -183,19 +252,46 @@ CREATE TABLE `likegroups` (
 --
 
 INSERT INTO `likegroups` (`c_id`, `g_name`) VALUES
-(1, 'oil'),
-(2, 'kolaj'),
-(2, 'oil'),
-(2, 'pastel'),
-(2, 'sketch'),
-(2, 'water'),
-(3, 'caltural'),
-(3, 'God'),
-(3, 'Indian history'),
-(4, 'abstract Painting'),
-(4, 'kolaj painting'),
-(4, 'real life'),
-(4, 'Real Object');
+(5, 'acrylic'),
+(5, 'Fine Art'),
+(5, 'Indian Culture'),
+(5, 'Mixed tempura on Sanganer handmade paper'),
+(6, 'Abstractionism'),
+(6, 'oil'),
+(6, 'Pastel'),
+(6, 'Sketch'),
+(7, 'bronze'),
+(7, 'Forest'),
+(7, 'kolaj'),
+(7, 'oil'),
+(7, 'pastel'),
+(7, 'sketch'),
+(7, 'water'),
+(8, 'Girl'),
+(8, 'God'),
+(8, 'Joy'),
+(8, 'Mixed tempura on Sanganer handmade paper'),
+(8, 'oil'),
+(8, 'Sketch'),
+(9, 'Girl'),
+(9, 'God'),
+(9, 'Joy'),
+(9, 'Mixed tempura on Sanganer handmade paper'),
+(9, 'oil'),
+(9, 'Pastel'),
+(9, 'Sketch'),
+(10, 'Abstractionism'),
+(10, 'acrylic'),
+(10, 'Nature'),
+(10, 'oil'),
+(10, 'River'),
+(11, 'Girl'),
+(11, 'God'),
+(11, 'Joy'),
+(11, 'Mixed tempura on Sanganer handmade paper'),
+(11, 'oil'),
+(11, 'Pastel'),
+(11, 'Sketch');
 
 -- --------------------------------------------------------
 
@@ -213,16 +309,33 @@ CREATE TABLE `prefers` (
 --
 
 INSERT INTO `prefers` (`a_name`, `c_id`) VALUES
-('Jaminy Roy', 1),
-('Jaminy Roy', 2),
-('Raja Ravi Varma', 2),
-('Nandalal Bose', 2),
-('Jaminy Roy', 3),
-('Jaminy Roy', 4),
-('Antonio Lopez', 4),
-('David Downton', 4),
-('Leonardo da Vinci', 4),
-('Pablo Picasso', 4);
+('Poorva Shingre', 5),
+('Jamini Roy', 5),
+('Nandalal Bose', 5),
+('Ram Kumar', 5),
+('Jaminy Roy', 6),
+('Sakti Burman', 6),
+('Nandalal Bose', 6),
+('Nandalal Bose', 7),
+('Sayed Haider Raza', 7),
+('Subodh Gupta', 7),
+('Jaminy Roy', 8),
+('Antonio Lopez', 8),
+('David Downton', 8),
+('Leonardo da Vinci', 8),
+('Pablo Picasso', 8),
+('Poorva Shingre', 9),
+('Jamini Roy', 9),
+('Nandalal Bose', 9),
+('Ram Kumar', 9),
+('Jamini Roy', 10),
+('MF Hussain', 10),
+('Nandalal Bose', 10),
+('Poorva Shingre', 10),
+('Poorva Shingre', 11),
+('Jamini Roy', 11),
+('Nandalal Bose', 11),
+('Ram Kumar', 11);
 
 -- --------------------------------------------------------
 
@@ -241,19 +354,21 @@ CREATE TABLE `transactions` (
 --
 
 INSERT INTO `transactions` (`txn_id`, `t_date`, `c_id`) VALUES
-(10, '2021-04-25', 2),
-(11, '2021-04-26', 1),
-(12, '2021-04-26', 4),
-(13, '2021-04-26', 4),
-(14, '2021-04-26', 3),
-(15, '2021-04-26', 3),
-(16, '2021-04-26', 2),
-(17, '2021-04-26', 1),
-(18, '2021-04-26', 1),
-(19, '2021-04-26', 2),
-(20, '2021-04-26', 2),
-(21, '2021-04-27', 3),
-(22, '2021-04-27', 3);
+(27, '2012-04-29', 7),
+(28, '2021-04-29', 8),
+(29, '2020-04-25', 5),
+(30, '2020-04-29', 6),
+(31, '2019-12-11', 6),
+(32, '2018-04-25', 5),
+(33, '2016-06-09', 5),
+(34, '2015-07-01', 5),
+(35, '2015-06-29', 9),
+(36, '2015-06-15', 10),
+(37, '2013-10-31', 9),
+(38, '2012-12-12', 6),
+(39, '2011-10-01', 7),
+(40, '2009-10-29', 7),
+(41, '2009-08-30', 9);
 
 --
 -- Indexes for dumped tables
@@ -321,13 +436,13 @@ ALTER TABLE `transactions`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `txn_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `txn_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- Constraints for dumped tables
